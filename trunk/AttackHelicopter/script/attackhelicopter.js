017624a767d668677682f2f243303871d4fd2958
skipperkongen <skipperkongen@13c2b3d8-82e2-2f1b-7222-5bc54bd83e26>
2011-02-08 00:10:35 +0000

    beta done

--- trunk/AttackHelicopter/script/attackhelicopter.js
+++ trunk/AttackHelicopter/script/attackhelicopter.js
@@ -72,7 +72,9 @@ var invasion = (function ($) {
 		
 		self.radio = null;
 		
-		self.helisound = null;
+		self.wagner = null;
+		
+		//self.helisound = null;
 		
 		self.step = function() {
 			// hide old frame
@@ -118,10 +120,12 @@ var invasion = (function ($) {
 				// make audio elements and add to body
 				self.guns = $('<audio preload="auto" src="audio/gun.mp3" />').addClass(heliID)[0];
 				self.radio = $('<audio preload="auto" src="audio/radio.mp3" />').addClass(heliID)[0];
-				self.helisound = $('<audio preload="auto" src="audio/heli.mp3" loop="loop" />').addClass(heliID)[0];
+				self.wagner = $('<audio preload="auto" src="audio/wagner_comp.mp3" loop="loop" autoplay="autoplay"/>').addClass(heliID)[0];
+				//self.helisound = $('<audio preload="auto" src="audio/heli.mp3" loop="loop" autoplay="autoplay"/>').addClass(heliID)[0];
 				$("body").append(self.guns);
 				$("body").append(self.radio);
-				$("body").append(self.helisound);
+				//$("body").append(self.helisound);
+				$("body").append(self.wagner);
 				
 				// create helicopter container
 				self.container = $("<div/>").addClass(heliID);
@@ -134,7 +138,7 @@ var invasion = (function ($) {
 					// style="font-weight: bold; font-size: 12px; font-familiy: monospace;"
 					var pre = $('<pre />').addClass(heliID).hide();
 					self.frames.push(pre);
-					$("#" + heliID).append(pre);
+					$("div." + heliID).append(pre);
 					var lines = frameLines[frame];
 					for(var line in lines) {
 						pre.append(lines[line] + '\n');
@@ -154,22 +158,36 @@ var invasion = (function ($) {
 			// select a random html element that has no children, and is a child of body
 			var targets = $("body *:not(." + heliID + "):not(:has(*))");
 			var rand = Math.floor(Math.random() * targets.length);
-			var target = targets.length == 0 ? null : targets[rand]; 
+			var target = targets.length == 0 ? targets : $(targets[rand]); 
 			return target;
 		}
 		
-		self.attack = function (target) {
+		self.attack = function () {
 			// 1: acquire a target
 			var target = self.acquireTarget();
-			// 2: move to target
-			self.moveRight(1, function() {
-				self.moveDown(1, function() {
+			if(target.length == 0) {
+				self.container.animate({"left": "100px", "top":"100px"}, 2000);
+				$('body').append("Victory!"); 
+				return;
+			}
+			var targetX = target.position().left;
+			var targetY = target.position().top + target.height()/2;
+			var gunX = self.container.position().left + self.container.width()/2;
+			var gunY = self.container.position().top + self.container.height()/2;
+			var deltaX = targetX - gunX;
+			var deltaY = targetY - gunY;
+			// 2: move to target. Lower right should be 10 px from target
+			self.moveRight(deltaX, function() {
+				self.moveDown(deltaY, function() {
 					// 3: Radio target to base
 					self.useRadio(function(){
 						// 4: Shoot the target
 						self.useGun(function() {
 							// 5: Remove target from DOM
-							$(target).fadeOut( function() { $(this).remove();
+							$(target).fadeOut( function() { 
+								$(this).remove();
+								// 6: Call attack again on finished
+								self.attack();
 							});
 						});
 					});				
@@ -189,22 +207,23 @@ var invasion = (function ($) {
 		}
 		
 		self.moveRight = function (distance, callback) {
-			self.container.animate({"left":"+="+distance+"px"}, 2000, callback);
+			self.container.animate({"left":"+="+distance+"px"}, 1500, callback);
 		};
 		
 		self.moveLeft = function (distance, callback) {
-			self.container.animate({"left":"-="+distance+"px"}, 2000, callback);
+			self.container.animate({"left":"-="+distance+"px"}, 1500, callback);
 		};
 		
 		self.moveUp = function (distance, callback) {
-			self.container.animate({"top":"-="+distance+"px"}, 2000, callback);
+			self.container.animate({"top":"-="+distance+"px"}, 1500, callback);
 		};
 		
 		self.moveDown = function (distance, callback) {
-			self.container.animate({"top":"+="+distance+"px"}, 2000, callback);
+			self.container.animate({"top":"+="+distance+"px"}, 1500, callback);
 		};
 		
 		self.init();
+		self.attack();
 	};
 	
 	return invasion;
